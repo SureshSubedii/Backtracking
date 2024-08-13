c141fd1a4dd63d8753a7e1469a97749bf5601c97
sureshsubedii <sureshsubedi485@gmail.com>
2024-08-13 16:39:35 +0545

    Combination Sum II

---
+++ combination_sum_II.js
@@ -0,0 +1,27 @@
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+const combinationSum2 = (candidates, target)=> {
+    let combinations = []
+     const backtrack = (start, path , target) =>{
+        if(target === 0){
+            combinations.push([...path])
+        }
+        if(target < 0) return
+
+        for(let i = start; i < candidates.length; i++){
+            if( i > start && candidates[i] === candidates[i - 1] ) continue // to skip duplicates
+            path.push(candidates[i])
+            backtrack(i + 1, path, target - candidates[i] )
+            path.pop()
+
+        }
+
+     }
+     backtrack(0, [], target)
+     return combinations
+
+}
