4555126c47a96c685a02dc752a0c1d284189a59a
suresh_11 <sureshsubedi485@gmail.com>
2024-08-29 18:01:12 +0545

    Revise BInary Watch

--- binary_watch.js
+++ binary_watch.js
@@ -1,17 +1,17 @@
 // A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
-var readBinaryWatch = function(num) {
-    const times = [];
-    for (let h = 0; h < 12; h++) {
-      for (let m = 0; m < 60; m++) {
-        const hOnes = h ? h.toString(2).match(/1/g).length : 0;
-        const mOnes = m ? m.toString(2).match(/1/g).length : 0;
-        if (hOnes + mOnes === num) {
-          times.push(`${h}:${m < 10 ? `0${m}` : m}`);
-        }
+var readBinaryWatch = function(turnedOn) {
+  let result = []
+  for(let i = 0; i < 12; i ++){
+      for(let j = 0; j < 60; j ++){
+          let hourOne = i.toString(2).match(/1/g)?.length || 0
+          let minuteOne = j.toString(2).match(/1/g)?.length || 0
+          if(hourOne + minuteOne === turnedOn) result.push(`${i}:${j < 10? 0: ""}${j}`)
+
       }
-    }
-    return times;
-  };
+  }
+  return result
+  
+};
 
 
 // Using DFS 
